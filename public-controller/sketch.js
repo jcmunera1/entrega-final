d691a17e0ba05e3cc58b1eb0e656412ebb1eb998
Juan Camilo <jcmunera1@gmail.com>
2022-06-02 21:32:54 -0500

    actualizado framer

--- public-controller/sketch.js
+++ public-controller/sketch.js
@@ -1,5 +1,14 @@
 
 //Create the socket
+let startTime = 0;
+let startInteraction = 0;
+let endInteraction = 0;
+let date = new Date();
+let [month, day, year] = [date.getMonth() + 1, date.getDate(), date.getFullYear()];
+let [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+let userInput;
+let sizeButton = 60;
+let hasStart = false;
 
 let participantEndpoint = 'http://localHost:5050/participant';
 
@@ -31,7 +40,19 @@ let onScreen = false;
 
 let endingImg;
 
-function preload() { 
+let myEmailJSON = {
+    /*mail: myEmail,
+    date: Date.now()*/
+    "lead": false,
+    "date": "",
+    "name": "",
+    "email": "",
+    "start": "",
+    "location": "Unicentro",
+    "timestamp": ""
+}
+
+function preload() {
     templateImg = loadImage('assets/spotifyTemplate.png');
     qrReader = loadImage('assets/qrreader.png');
     joiningImg = loadImage('assets/joining.png');
@@ -46,41 +67,52 @@ function setup() {
 
     montserrateFont = loadFont('assets/Montserrat-Bold.otf');
 
-  emailInput = createInput('');
-  emailInput.position(170, 1400);
-  emailInput.size(800, 90);
-  emailInput.input(myInputEmail);
-   
+    emailInput = createInput('');
+    emailInput.position(170, 1400);
+    emailInput.size(800, 90);
+    emailInput.input(myInputEmail);
+
 
 }
-
+function startButtonHotSpot(element) {
+    if (dist(pmouseX, pmouseY, screens.posX, screens.posY) < sizeButton) {
+        console.log('start interaction');
+        startTime = `${hour}:${minutes}:${seconds}`;
+        startInteraction = Date.now();
+        hasStart = true;
+        userInput.style('display', 'block');
+    }
+}
 
 
 function draw() {
 
     background(0);
 
-    if (timer > 26) { 
+    if (timer > 26) {
         screens = 3;
     }
 
-    switch (screens) { 
+    switch (screens) {
         case 0:
             image(qrReader, 0, 0);
             emailInput.style('display', 'none');
+            //startButtonHotSpot(element);
+
+
             break;
-        
+
         case 1:
-            image(joiningImg, 0, 0); 
+            image(joiningImg, 0, 0);
             emailInput.style('display', 'none');
             break;
-        
+
         case 2:
-            image(finalImg, 0, 0); 
+            image(finalImg, 0, 0);
             emailInput.style('display', 'block');
             break;
         case 3:
-            image(endingImg, 0, 0); 
+            image(endingImg, 0, 0);
             emailInput.style('display', 'none');
 
             fill(255);
@@ -90,60 +122,74 @@ function draw() {
             break;
     }
 
-    
 
-   
+
+
 }
 
 
 
-function mouseClicked() { 
+function mouseClicked() {
 
     //Checks if button was pressed, if so, send the string inside of it to the array of words selected.
 
-    switch (screens) { 
+    switch (screens) {
         case 0:
-            if (dist(mouseX, mouseY, 700, 1300)<600) { 
+            if (dist(mouseX, mouseY, 700, 1300) < 600) {
                 screens = 1;
             }
             break;
-        
-         case 1:
-            if (dist(mouseX, mouseY, 570, 1800)<600) { 
+
+        case 1:
+            if (dist(mouseX, mouseY, 570, 1800) < 600) {
                 screens = 2;
             }
             break;
         case 2:
-             if (dist(mouseX, mouseY, 570, 2000)<300) { 
-                 screens = 3;
-                 let myEmailJSON = {
-                     mail: myEmail,
-                     date: Date.now()
-                 }
-                 socket.emit('participant', myEmailJSON);
-                 //sendParticipant(myEmail);
-                 console.log("undiu")
+            if (dist(mouseX, mouseY, 570, 2000) < 300) {
+                screens = 3;
+                endInteraction = Math.floor((Date.now() - startInteraction) / 1000)
+                hasStart = false;
+                if (myEmailJSON.myEmail != '') {
+                    myEmailJSON = {
+                        /*mail: myEmail,
+                        date: Date.now()*/
+                        "lead": true,
+                        "date": `${month}/${day}/${year}`,
+                        "name": myEmail,
+                        "email": `${myEmail}@gmail.com`,
+                        "start": startTime,
+                        "location": "Boulevard",
+                        "timestamp": endInteraction
+                    }
+                } else {
+                    
+
+                }
+                socket.emit('participant', myEmailJSON);
+                //sendParticipant(myEmail);
+                console.log("undiu")
             }
             break;
-        
+
     }
 }
 
-function myInputEmail(){
+function myInputEmail() {
     myEmail = this.value();
 }
 
-function sendToArray(object, min, max, pos) { 
-    for (let i = 0; i < object.length; i++) { 
+function sendToArray(object, min, max, pos) {
+    for (let i = 0; i < object.length; i++) {
         if (timer > min && timer < max) {
             compareDistance(object[i], pos);
-         }    
-     }
+        }
+    }
 }
 
-function showIntent(object, min, max) { 
+function showIntent(object, min, max) {
 
-    for (let i = 0; i < object.length; i++) { 
+    for (let i = 0; i < object.length; i++) {
 
         if (timer > min && timer < max) {
             object[i].show(montserrateFont);
@@ -152,8 +198,8 @@ function showIntent(object, min, max) {
     }
 }
 
-function compareDistance(object, pos) { 
-    if (mouseX > object.getX() && mouseX < (object.getX() + 600) && mouseY > object.getY() && mouseY < (object.getY() + 200)) { 
+function compareDistance(object, pos) {
+    if (mouseX > object.getX() && mouseX < (object.getX() + 600) && mouseY > object.getY() && mouseY < (object.getY() + 200)) {
         object.setPressed(true);
 
         myWords[pos] = object.getText();
@@ -162,14 +208,14 @@ function compareDistance(object, pos) {
     }
 }
 
-function counter() { 
+function counter() {
 
-    if (frameCount % 15 === 0 ) { 
-      timer++;
-      console.log(timer)
+    if (frameCount % 15 === 0) {
+        timer++;
+        console.log(timer)
     }
 }
-  
+
 async function sendParticipant(participant) {
     let bodyJSON = JSON.stringify(participant);
     const postRequest = {
@@ -181,4 +227,3 @@ async function sendParticipant(participant) {
     }
     const request = await fetch(participantEndpoint, postRequest);
 }
-  
